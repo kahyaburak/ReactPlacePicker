cf184e573000e5942cbdaa9156521c70a56d0da2
Burak <burakkahyabk@gmail.com>
2024-06-12 17:48:37 +0300

    useEffect, side effects both with and without using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useRef, useState } from 'react';
+import {useRef, useState, useEffect} from 'react';
 
 import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
+import {AVAILABLE_PLACES} from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import {sortPlacesByDistance} from './loc.js';
 
 function App() {
+  const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+  const storedPlaces = storedIds.map((id) =>
+    AVAILABLE_PLACES.find((place) => place.id === id)
+  );
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+  useEffect(() => {
+    let sortedPlaces = [];
+    navigator.geolocation.getCurrentPosition((position) => {
+      sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,6 +46,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        'selectedPlaces',
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -35,6 +61,13 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+
+    localStorage.removeItem(
+      'selectedPlaces',
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
@@ -63,7 +96,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText={'Sorting places by distance...'}
           onSelectPlace={handleSelectPlace}
         />
       </main>
--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,8 +1,8 @@
-export default function Places({ title, places, fallbackText, onSelectPlace }) {
+export default function Places({title, places, fallbackText, onSelectPlace}) {
   return (
     <section className="places-category">
       <h2>{title}</h2>
-      {places.length === 0 && <p className='fallback-text'>{fallbackText}</p>}
+      {places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
       {places.length > 0 && (
         <ul className="places">
           {places.map((place) => (
