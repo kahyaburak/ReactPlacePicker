f57f05230cdc1e681da1e7ce8fa3d6816a9cacb7
Burak <burakkahyabk@gmail.com>
2024-06-13 15:22:39 +0300

    Added A Progress Bar

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,13 +1,25 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({onConfirm, onCancel}) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
   useEffect(() => {
-    console.log('timer set');
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
     return () => {
-      console.log('timer done');
       clearTimeout(timer);
     };
   }, [onConfirm]);
@@ -23,6 +35,7 @@ export default function DeleteConfirmation({onConfirm, onCancel}) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
